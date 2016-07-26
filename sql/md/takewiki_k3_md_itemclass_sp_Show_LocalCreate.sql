f8d3355c7a3a49fea651447ec7c8b732172641ce
takewiki <hawken.hu@takewiki.com>
2016-07-26 20:01:58 +0800

    create the proc 'takewiki_k3_md_itemclass_sp_Show'
    create the proc 'takewiki_k3_md_itemclass_sp_Show' to institute the
    select statement.
    the usage is
    exec takewiki_k3_md_itemclass_sp_Show

---
+++ sql/md/takewiki_k3_md_itemclass_sp_Show_LocalCreate.sql
@@ -0,0 +1,11 @@
+﻿if OBJECT_ID(N'takewiki_k3_md_itemclass_sp_Show') is not null
+begin 
+drop proc takewiki_k3_md_itemclass_sp_Show
+end
+go
+create proc takewiki_k3_md_itemclass_sp_Show
+as
+select * from  takewiki_k3_md_itemclass_view
+--usage
+--exec takewiki_k3_md_itemclass_sp_Show
+go

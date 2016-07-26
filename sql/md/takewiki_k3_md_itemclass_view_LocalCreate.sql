ee9de8fb83f3143f09c902ac8a996d43a741e242
takewiki <hawken.hu@takewiki.com>
2016-07-26 19:46:29 +0800

    add the view to the k3 itemclass
    wrapper the t_itemclass table into  a view called
    'takewiki_k3_md_itemclass_view' using the local database create method

---
+++ sql/md/takewiki_k3_md_itemclass_view_LocalCreate.sql
@@ -0,0 +1,9 @@
+﻿if OBJECT_ID(N'takewiki_k3_md_itemclass_view') is not null
+begin
+drop view takewiki_k3_md_itemclass_view 
+end
+go
+create view takewiki_k3_md_itemclass_view
+as
+select FItemClassID,FNumber,FName,FSQLTableName from t_ItemClass
+go

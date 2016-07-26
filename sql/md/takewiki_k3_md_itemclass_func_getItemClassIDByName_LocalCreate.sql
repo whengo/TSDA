a6303ff3ab97ac1ba80535e23420b45eae2ea314
takewiki <hawken.hu@takewiki.com>
2016-07-26 21:44:52 +0800

    add the function 'takewiki_k3_md_itemclass_func_getItemClassIDByName'
    add the function 'takewiki_k3_md_itemclass_func_getItemClassIDByName' to
    substitute the select  fitemclassid from t_itemclassid  and the usage is
    as follows
    select dbo.takewiki_k3_md_itemclass_func_getItemClassIDByName('供应商') and
    the function is just like the R function but in the sql,function is
    strictly setup for using the query  not the sub_proc

---
+++ sql/md/takewiki_k3_md_itemclass_func_getItemClassIDByName_LocalCreate.sql
@@ -0,0 +1,17 @@
+﻿
+if  object_id(N'takewiki_k3_md_itemclass_func_getItemClassIDByName') is not null
+begin
+drop function takewiki_k3_md_itemclass_func_getItemClassIDByName
+end
+go
+create function takewiki_k3_md_itemclass_func_getItemClassIDByName (@FName nvarchar(30))
+returns int
+as
+begin
+declare @FItemClassID int
+select  @FitemClassID= FItemClassid from takewiki_k3_md_itemclass_view
+where FName =@FName
+return @FItemClassID
+end
+go
+---select dbo.takewiki_k3_md_itemclass_func_getItemClassIDByName('供应商')

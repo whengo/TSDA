23c241cf620793155febcbe242bcc24ce718017d
takewiki <hawken.hu@takewiki.com>
2016-07-26 22:03:10 +0800

    add function 'takewiki_k3_md_itemclass_func_getSQLTableNameByName'
    add function 'takewiki_k3_md_itemclass_func_getSQLTableNameByName' to
    get the sql table name for each master data(by its name)

---
+++ sql/md/takewiki_k3_md_itemclass_func_getSQLTableNameByName_LocalCreate.sql
@@ -0,0 +1,16 @@
+﻿if  object_id(N'takewiki_k3_md_itemclass_func_getSQLTableNameByName') is not null
+begin
+drop function takewiki_k3_md_itemclass_func_getSQLTableNameByName
+end
+go
+create function takewiki_k3_md_itemclass_func_getSQLTableNameByName (@FName nvarchar(30))
+returns nvarchar(50)
+as
+begin
+declare @SQLTableName nvarchar(50)
+select  @SQLTableName= FSQLTableName from takewiki_k3_md_itemclass_view
+where FName =@FName
+return @SQLTableName
+end
+go
+---select dbo.takewiki_k3_md_itemclass_func_getSQLTableNameByName('供应商')

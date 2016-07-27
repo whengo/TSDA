7d695a41005a6a20566f83af92f4c56e6029dc05
takewiki <hawken.hu@takewiki.com>
2016-07-27 22:17:03 +0800

    add the func takewiki_k3_md_account_func_getNameByItemID
    get the accont name by account id

---
+++ sql/md/takewiki_k3_md_account_func_getNameByItemID_LocalCreate.sql
@@ -0,0 +1,18 @@
+﻿if object_id('takewiki_k3_md_account_func_getNameByItemID') is not null
+drop function  takewiki_k3_md_account_func_getNameByItemID
+go
+create function takewiki_k3_md_account_func_getNameByItemID
+(@FItemID int) returns nvarchar(80)
+as
+begin
+  declare @FName nvarchar(80)
+
+select @FName =FName from takewiki_k3_md_account_view
+   where FItemID =@FItemID
+   return @FName
+end
+go
+--usage
+
+-- select dbo.takewiki_k3_md_account_func_getNameByItemID(1001)
+-- select dbo.takewiki_k3_md_account_func_getNameByItemID(1091)

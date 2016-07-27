d86698dcc8716647494f91a5bd86c5f8e878cf53
takewiki <hawken.hu@takewiki.com>
2016-07-27 21:26:32 +0800

    add the func 'takewiki_k3_md_account_func_getItemIDByLevel'
    get the account id by the level which can be used to create the table
    hiarerchy for the mdx query

---
+++ sql/md/takewiki_k3_md_account_func_getItemIDByLevel_LocalCreate.sql
@@ -0,0 +1,25 @@
+﻿if object_id('takewiki_k3_md_account_func_getItemIDByLevel') is not null
+drop function  takewiki_k3_md_account_func_getItemIDByLevel
+go
+create function takewiki_k3_md_account_func_getItemIDByLevel
+(@FItemID int,@FLevel int) returns int
+as
+begin
+declare @LevelItemID int
+  if dbo.takewiki_k3_md_account_func_getCurrentLevel(@FItemID) = @FLevel
+  begin
+   select @LevelItemID = @FItemid
+   end
+   else
+   begin
+   select @LevelItemID = dbo.takewiki_k3_md_account_func_getParentIDByLevel(@FItemID,@Flevel)
+   end
+   return @LevelItemID
+
+end
+go
+--usage
+-- select dbo.takewiki_k3_md_account_func_getItemIDByLevel(1106,3)
+-- select dbo.takewiki_k3_md_account_func_getItemIDByLevel(1106,2)
+-- select dbo.takewiki_k3_md_account_func_getItemIDByLevel(1106,1)
+-- select dbo.takewiki_k3_md_account_func_getItemIDByLevel(1106,4)

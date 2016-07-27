52683287a8c588820638a0cbbe475f8272e2146d
takewiki <hawken.hu@takewiki.com>
2016-07-27 19:14:08 +0800

    test the view with the base view changed
    we should rebuild the view when the base view 'takewiki_k3_md_coa_view'
    changed

--- sql/md/takewiki_k3_md_account_view_LocalCreate.sql
+++ sql/md/takewiki_k3_md_account_view_LocalCreate.sql
@@ -1,4 +1,4 @@
-﻿if object_id('takewiki_k3_md_account_view') is not null
+if object_id('takewiki_k3_md_account_view') is not null
 drop view takewiki_k3_md_account_view
 go
 create view takewiki_k3_md_account_view
@@ -9,3 +9,5 @@ go
 --note by hawken.hu form takewiki
 ---select *  from  takewiki_k3_md_coa_view
 ---the two view is just has the same function
+--视图不会自动更新，尤其是当takewiki_k3_md_coa_view的结构发生改变时
+--因此需要重运行此脚本，相当于升级了一次，这个是一个bug

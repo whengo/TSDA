513418f2a621f1704dba63f53f99187bab9ab8d0
takewiki <hawken.hu@takewiki.com>
2016-07-27 13:13:16 +0800

    add proc takewiki_k3_md_itemclass_sp_getCountByName
    using the sp_executesql  to deal with the proc creation
    usage is
    -declare @count int
    ---exec takewiki_k3_md_itemclass_sp_getCountByName N'客户',@count out
    --select @count

---
+++ sql/md/takewiki_k3_md_itemclass_sp_getCountByName_LocalCreate.sql
@@ -0,0 +1,13 @@
+﻿
+create proc takewiki_k3_md_itemclass_sp_getCountByName(@FName nvarchar(30),@Count int out)
+as
+begin
+declare @sql nvarchar(400)
+select  @sql =N'select  @count = count(1) from takewiki_k3_md_itemclass_view where FName =@FName';
+exec sp_executesql @sql,N'@count int out,@FName nvarchar(30)',@Count out,@FName
+end
+--Usage 
+
+--declare @count int 
+---exec takewiki_k3_md_itemclass_sp_getCountByName N'客户',@count out
+--select @count

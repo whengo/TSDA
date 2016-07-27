7903f480b710c555c144183ce9d80ebaae372e40
takewiki <hawken.hu@takewiki.com>
2016-07-27 21:49:51 +0800

    add the func takewiki_k3_md_account_func_getItemIDByName
    get the account id by name in the contion the name is unique,if the name
    is not unique , it will return an error!

---
+++ sql/md/takewiki_k3_md_account_func_getItemIDByName_LocalCreate.sql
@@ -0,0 +1,20 @@
+﻿if object_id('takewiki_k3_md_account_func_getItemIDByName') is not null
+drop function  takewiki_k3_md_account_func_getItemIDByName
+go
+create function takewiki_k3_md_account_func_getItemIDByName
+(@FName nvarchar(80)) returns int
+as
+begin
+  declare @FItemID int
+
+select @FItemID =FItemID from takewiki_k3_md_account_view
+   where FName =@FName
+   return @FItemID
+end
+go
+--usage
+
+-- select dbo.takewiki_k3_md_account_func_getItemIDByName('商誉')
+-- select dbo.takewiki_k3_md_account_func_getItemIDByName('短期借款')
+
+

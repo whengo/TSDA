1afe43030515e057b1bcb51e04ea94195c0b9c36
takewiki <hawken.hu@takewiki.com>
2016-07-27 18:14:02 +0800

    add the base proc to the takewiki core part
    query the table with the sql tablename,you can spefify the local
    database or remote database,the defalut schema is dbo and the default
    field list is all,but you can also set the filed list on your hehalf

---
+++ sql/core/takewiki_QueryTable_CreateScript.sql
@@ -0,0 +1,33 @@
+﻿if object_id('takewiki_QueryTable') is not null
+   drop proc  takewiki_QueryTable
+go
+create  proc takewiki_QueryTable
+(
+@TableName nvarchar (30) ='t_icitem',
+@FieldList nvarchar (400)='*',
+@DBName nvarchar(50)='',
+@DBType nvarchar(30)='K3ERP', 
+@Schema nvarchar (30)='dbo')
+as
+
+set nocount on 
+begin
+declare @sql nvarchar(max)=N''
+if @DBName =''
+  begin
+  select @DBName = db_name()
+  end
+if @DBType = 'K3ERP'
+begin
+select @sql=N'select '+@FieldList+N'  from  '+@DBName+N'.'+@Schema+N'.'+@TableName 
+end
+exec sp_executesql @sql
+end
+go
+--usage
+
+-- exec takewiki_QueryTable 'icstockbill','*','AIS20160721134104'
+-- exec takewiki_QueryTable 'icstockbill','*'
+-- exec takewiki_QueryTable 'icstockbill','*','AIS20160721134104'
+-- exec takewiki_QueryTable 'icstockbillentry','*'
+-- exec takewiki_QueryTable 't_account','faccountid,fnumber,fname','AIS20141027173850'

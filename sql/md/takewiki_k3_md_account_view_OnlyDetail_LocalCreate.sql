132c5ec61cc10bcbf99082f2e3fdfe935d4a38cd
takewiki <hawken.hu@takewiki.com>
2016-07-27 23:02:13 +0800

    add the view 'takewiki_k3_md_account_view_OnlyDetail'
    get the account only in the detail ones

---
+++ sql/md/takewiki_k3_md_account_view_OnlyDetail_LocalCreate.sql
@@ -0,0 +1,10 @@
+﻿if object_id('takewiki_k3_md_account_view_OnlyDetail') is not null
+drop view takewiki_k3_md_account_view_OnlyDetail
+go
+create view takewiki_k3_md_account_view_OnlyDetail
+as
+select  * from takewiki_k3_md_account_view
+where fdetail=1
+go
+
+--takewiki_QueryTable 'takewiki_k3_md_account_view_OnlyDetail'

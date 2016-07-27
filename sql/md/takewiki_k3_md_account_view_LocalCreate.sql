fcae81ed212c92f4ce2091a6e9dd88d3e336b0bb
takewiki <hawken.hu@takewiki.com>
2016-07-27 18:56:03 +0800

    syn the view takewiki_k3_md_coa_view with alias name 'takewiki_k3_md_account_view'
    get the definition the view takewiki_k3_md_coa_view and change the name
    with 'takewiki_k3_md_coa_view'

---
+++ sql/md/takewiki_k3_md_account_view_LocalCreate.sql
@@ -0,0 +1,11 @@
+﻿if object_id('takewiki_k3_md_account_view') is not null
+drop view takewiki_k3_md_account_view
+go
+create view takewiki_k3_md_account_view
+as
+select *  from  takewiki_k3_md_coa_view
+go
+---select * from takewiki_k3_md_account_view
+--note by hawken.hu form takewiki
+---select *  from  takewiki_k3_md_coa_view
+---the two view is just has the same function

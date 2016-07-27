aca0ed64b5ab7f7b13d7dbee5197c62b41b83e39
takewiki <hawken.hu@takewiki.com>
2016-07-27 22:50:33 +0800

    add the func takewiki_k3_md_account_func_getFullNameByItemID
    get the account full name by the account id

---
+++ sql/md/takewiki_k3_md_account_func_getFullNameByItemID_LocalCreate.sql
@@ -0,0 +1,16 @@
+﻿if object_id('takewiki_k3_md_account_func_getFullNameByItemID') is not null
+drop function takewiki_k3_md_account_func_getFullNameByItemID
+go
+create function takewiki_k3_md_account_func_getFullNameByItemID
+(@FItemID int) returns nvarchar(250)
+as
+begin
+declare @FFullName nvarchar(250)
+   select @FFullName = FFullName from takewiki_k3_md_account_view_Ext1
+   where FItemID = @FItemID
+   return @FFullName
+end
+go
+--usage
+
+-- select dbo.takewiki_k3_md_account_func_getFullNameByItemID(1099)

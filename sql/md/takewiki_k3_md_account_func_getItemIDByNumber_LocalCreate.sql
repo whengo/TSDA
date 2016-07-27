4eedad5f498a1b86c0a433137ca4ae9f0ae70521
takewiki <hawken.hu@takewiki.com>
2016-07-27 21:44:14 +0800

    add the func  'takewiki_k3_md_account_func_getItemIDByNumber'
    get the account id by account number

---
+++ sql/md/takewiki_k3_md_account_func_getItemIDByNumber_LocalCreate.sql
@@ -0,0 +1,18 @@
+﻿if object_id('takewiki_k3_md_account_func_getItemIDByNumber') is not null
+drop function  takewiki_k3_md_account_func_getItemIDByNumber
+go
+create function takewiki_k3_md_account_func_getItemIDByNumber
+(@FNumber nvarchar(30)) returns int
+as
+begin
+  declare @FItemID int
+
+select @FItemID =FItemID from takewiki_k3_md_account_view
+   where FNumber =@FNumber
+   return @FItemID
+end
+go
+--usage
+
+-- select dbo.takewiki_k3_md_account_func_getItemIDByNumber('6101.02')
+-- select dbo.takewiki_k3_md_account_func_getItemIDByNumber('1001')

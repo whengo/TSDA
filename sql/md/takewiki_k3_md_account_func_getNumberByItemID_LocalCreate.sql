f773b53ecc87d5bb3e6737eb153870865d545515
takewiki <hawken.hu@takewiki.com>
2016-07-27 22:20:40 +0800

    add the func 'takewiki_k3_md_account_func_getNumberByItemID'
    get the account number by the account id use this.function

---
+++ sql/md/takewiki_k3_md_account_func_getNumberByItemID_LocalCreate.sql
@@ -0,0 +1,18 @@
+﻿if object_id('takewiki_k3_md_account_func_getNumberByItemID') is not null
+drop function takewiki_k3_md_account_func_getNumberByItemID
+go
+create function takewiki_k3_md_account_func_getNumberByItemID
+(@FItemID int) returns nvarchar(30)
+as
+begin
+  declare @FNumber nvarchar(30)
+
+select @FNumber =FNumber from takewiki_k3_md_account_view
+   where FItemID =@FItemID
+   return @FNumber
+end
+go
+--usage
+
+-- select dbo.takewiki_k3_md_account_func_getNumberByItemID(1001)
+-- select dbo.takewiki_k3_md_account_func_getNumberByItemID(1091)

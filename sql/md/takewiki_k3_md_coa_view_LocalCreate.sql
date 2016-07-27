ffda0ede6b5a55fc04a21e7cfca52c6247611dfa
takewiki <hawken.hu@takewiki.com>
2016-07-27 18:47:04 +0800

    add the view 'takewiki_k3_md_coa_view'
    增加科目表的视图

---
+++ sql/md/takewiki_k3_md_coa_view_LocalCreate.sql
@@ -0,0 +1,9 @@
+﻿if object_id('takewiki_k3_md_COA_view') is not null
+drop view takewiki_k3_md_COA_view
+go
+create view takewiki_k3_md_COA_view
+as
+select faccountid,fnumber,fname,flevel,fdetail,fparentid,fdelete from t_account
+go
+---select *from takewiki_k3_md_coa_view
+

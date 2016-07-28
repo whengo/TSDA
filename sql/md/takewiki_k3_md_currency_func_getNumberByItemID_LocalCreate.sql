d0e4dd4f9da156896346b609ce1443f77397c60e
takewiki <hawken.hu@takewiki.com>
2016-07-28 22:57:57 +0800

    add the func takewiki_k3_md_currency_func_getNumberByItemID
    get the currency number by the currency id

---
+++ sql/md/takewiki_k3_md_currency_func_getNumberByItemID_LocalCreate.sql
@@ -0,0 +1,17 @@
+﻿if object_id('takewiki_k3_md_currency_func_getNumberByItemID') is not null
+drop function takewiki_k3_md_currency_func_getNumberByItemID
+go
+create function takewiki_k3_md_currency_func_getNumberByItemID
+(@FItemID int) returns nvarchar(30)
+as
+begin
+  declare @FNumber nvarchar(30)
+
+select @FNumber =FNumber from takewiki_k3_md_currency_view
+   where FItemID =@FItemID
+   return @FNumber
+end
+go
+--usage
+--  select dbo.takewiki_k3_md_currency_func_getNumberByItemID(1)
+--  select dbo.takewiki_k3_md_currency_func_getNumberByItemID(1001)

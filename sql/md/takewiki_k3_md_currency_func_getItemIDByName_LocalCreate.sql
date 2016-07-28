5e6a543f21c6d794419a9059efba276e886ea67b
takewiki <hawken.hu@takewiki.com>
2016-07-28 23:03:45 +0800

    add the func takewiki_k3_md_currency_func_getItemIDByName
    get the currency  id by the currency name

---
+++ sql/md/takewiki_k3_md_currency_func_getItemIDByName_LocalCreate.sql
@@ -0,0 +1,17 @@
+﻿if object_id('takewiki_k3_md_currency_func_getItemIDByName') is not null
+drop function  takewiki_k3_md_currency_func_getItemIDByName
+go
+create function takewiki_k3_md_currency_func_getItemIDByName
+(@FName nvarchar(80)) returns int
+as
+begin
+  declare @FItemID int
+
+select @FItemID =FItemID from takewiki_k3_md_currency_view
+   where FName =@FName
+   return @FItemID
+end
+go
+--usage
+select dbo.takewiki_k3_md_currency_func_getItemIDByName('人民币')
+select dbo.takewiki_k3_md_currency_func_getItemIDByName('日元')

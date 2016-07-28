400eb54b1e3b55ac4dda921591ec18a44314aa39
takewiki <hawken.hu@takewiki.com>
2016-07-28 23:20:23 +0800

    add the func takewiki_k3_md_currency_func_getScaleByItemID
    get the currency amount decimal by the currency id

---
+++ sql/md/takewiki_k3_md_currency_func_getScaleByItemID_LocalCreate.sql
@@ -0,0 +1,17 @@
+﻿if object_id('takewiki_k3_md_currency_func_getScaleByItemID') is not null
+drop function  takewiki_k3_md_currency_func_getScaleByItemID
+go
+create function takewiki_k3_md_currency_func_getScaleByItemID
+(@FItemID int) returns int
+as
+begin
+  declare @FScale int
+
+select @FScale =FScale from takewiki_k3_md_currency_view
+   where FItemID =@FItemID
+   return @FScale
+end
+go
+--usage
+--select dbo.takewiki_k3_md_currency_func_getScaleByItemID(1)
+--select dbo.takewiki_k3_md_currency_func_getScaleByItemID(1001)

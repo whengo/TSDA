ecaf8b7dc9976a407bdc2efc15cc1ed96a21b5a3
takewiki <hawken.hu@takewiki.com>
2016-07-29 14:28:44 +0800

    add the proc takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo
    --for the saleOutStock after hook with saleInovice,when unHook the
    SaleInvoice
    --should also Clear the hook qty on the saleoutstock
    --fix the but of k/3ERP standard function

---
+++ sql/scm/takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo_localCreate.sql
@@ -0,0 +1,22 @@
+﻿if object_id('takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo') is not null
+drop proc takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo
+go
+create proc takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo
+(@FBillNo nvarchar(30))
+as 
+--
+--exec takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo
+--for the saleOutStock after hook with saleInovice,when unHook the SaleInvoice
+--should also Clear the hook qty on the saleoutstock
+
+begin
+set nocount on
+update  a set  FAllHookQTY =0  from icstockbillentry a 
+inner join icstockbill b
+on a.finterid=b.finterid
+where b.fbillno =@FBillNo and b.ftrantype=21
+select 'updated' as result
+end
+
+--useage
+-- exec takewiki_k3_scm_SaleOutStock_proc_ClearHookQtyByBillNo 'XOUT000010'

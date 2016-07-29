1c8a79713b494a9e85f7cf223105cd2afc79f296
takewiki <hawken.hu@takewiki.com>
2016-07-29 22:13:41 +0800

    add the tsda for the project

---
+++ sql/core/tsda.sql
@@ -0,0 +1,26 @@
+if object_id('tsda') is not null
+   drop proc tsda
+go
+create  proc tsda
+(
+@TableName nvarchar (80) ='t_icitem',
+@FieldList nvarchar (400)='*',
+@DBName nvarchar(50)='',
+@DBType nvarchar(30)='K3ERP', 
+@Schema nvarchar (30)='dbo')
+as
+
+set nocount on 
+begin
+declare @sql nvarchar(max)=N''
+if @DBName =''
+  begin
+  select @DBName = db_name()
+  end
+if @DBType = 'K3ERP'
+begin
+select @sql=N'select '+@FieldList+N'  from  '+@DBName+N'.'+@Schema+N'.'+@TableName 
+end
+exec sp_executesql @sql
+end
+go

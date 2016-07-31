40dbceb6538dc12e0d43a48743affc0dc94ecc4c
takewiki <hawken.hu@takewiki.com>
2016-08-01 00:04:49 +0800

    add the func takewiki_k3_md_customer_func_getParentIDByLevel
    add the func takewiki_k3_md_customer_func_getParentIDByLevel

---
+++ sql/md/customer/takewiki_k3_md_customer_func_getParentIDByLevel_LocalCreate.sql
@@ -0,0 +1,10 @@
+﻿exec  tsda_funcFactory_getParentIDByLevel 'takewiki_k3_md_customer_func_getParentIDByLevel',
+'takewiki_k3_md_customer_view',
+'FItemID',
+'FParentID',
+'FLevel'
+
+--tsda 'takewiki_k3_md_customer_view'
+--select dbo.takewiki_k3_md_customer_func_getParentIDByLevel(388,3)
+--select dbo.takewiki_k3_md_customer_func_getParentIDByLevel(388,2)
+--select dbo.takewiki_k3_md_customer_func_getParentIDByLevel(388,1)

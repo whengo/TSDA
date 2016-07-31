7ec359080510923a3bac30a7f00d08e70feadb6f
takewiki <hawken.hu@takewiki.com>
2016-08-01 00:24:05 +0800

    add the func takewiki_k3_md_customer_func_getNameByItemID
    add the func takewiki_k3_md_customer_func_getNameByItemID

---
+++ sql/md/customer/takewiki_k3_md_customer_func_getNameByItemID_LocalCreate.sql
@@ -0,0 +1,9 @@
+﻿-- tsda 'takewiki_k3_md_department_view'
+tsda_funcFactory_getNameByItemID 'takewiki_k3_md_customer_func_getNameByItemID',
+'takewiki_k3_md_customer_view',
+'FItemID',
+'FName'
+
+--tsda takewiki_k3_md_customer_view
+--select dbo.takewiki_k3_md_customer_func_getNameByItemID(378)
+--select dbo.takewiki_k3_md_customer_func_getNameByItemID(379)

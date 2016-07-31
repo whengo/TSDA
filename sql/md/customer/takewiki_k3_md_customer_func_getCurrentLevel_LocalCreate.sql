d942229c927f762fac1a09b43bec656d805227e7
takewiki <hawken.hu@takewiki.com>
2016-08-01 00:12:55 +0800

    add the func takewiki_k3_md_customer_func_getCurrentLevel
    add the func takewiki_k3_md_customer_func_getCurrentLevel

---
+++ sql/md/customer/takewiki_k3_md_customer_func_getCurrentLevel_LocalCreate.sql
@@ -0,0 +1,7 @@
+﻿exec tsda_funcFactory_getCurrentLevel 
+'takewiki_k3_md_customer_func_getCurrentLevel',
+'takewiki_k3_md_customer_view','FItemID','FLevel'
+---tsda takewiki_k3_md_customer_view
+
+--select dbo.takewiki_k3_md_customer_func_getCurrentLevel(388)
+--select dbo.takewiki_k3_md_customer_func_getCurrentLevel(384)

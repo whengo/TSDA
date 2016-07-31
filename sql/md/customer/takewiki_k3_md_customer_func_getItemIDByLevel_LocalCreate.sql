31c66ecff02a9bf90c507314b48a2df0b5879834
takewiki <hawken.hu@takewiki.com>
2016-08-01 00:17:51 +0800

    add the func  takewiki_k3_md_customer_func_getItemIDByLevel

---
+++ sql/md/customer/takewiki_k3_md_customer_func_getItemIDByLevel_LocalCreate.sql
@@ -0,0 +1,9 @@
+﻿exec tsda_funcFactory_getItemIDByLevel 'takewiki_k3_md_customer_func_getItemIDByLevel',
+'takewiki_k3_md_customer_func_getCurrentLevel',
+'takewiki_k3_md_customer_func_getParentIDByLevel'
+
+---  tsda 'takewiki_k3_md_customer_view'
+ 
+ --  select dbo.takewiki_k3_md_customer_func_getItemIDByLevel(389,3)
+  --  select dbo.takewiki_k3_md_customer_func_getItemIDByLevel(389,2)
+   --  select dbo.takewiki_k3_md_customer_func_getItemIDByLevel(389,1)

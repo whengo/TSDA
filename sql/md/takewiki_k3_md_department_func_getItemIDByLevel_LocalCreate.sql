24826523254550e19e551affeb6414b2a1447feb
takewiki <hawken.hu@takewiki.com>
2016-07-31 13:14:12 +0800

    add the func takewiki_k3_md_department_func_getItemIDByLevel
    takewiki_k3_md_department_func_getItemIDByLevel
    use the core proc
    tsda_funcFactory_getItemIDByLevel

---
+++ sql/md/takewiki_k3_md_department_func_getItemIDByLevel_LocalCreate.sql
@@ -0,0 +1,8 @@
+﻿exec tsda_funcFactory_getItemIDByLevel 'takewiki_k3_md_department_func_getItemIDByLevel',
+'takewiki_k3_md_department_func_getCurrentLevel',
+'takewiki_k3_md_department_func_getParentIDByLevel'
+
+---  tsda 'takewiki_k3_md_department_view'
+ 
+ --  select dbo.takewiki_k3_md_department_func_getItemIDByLevel(258,2)
+  -- select dbo.takewiki_k3_md_department_func_getItemIDByLevel(258,1)

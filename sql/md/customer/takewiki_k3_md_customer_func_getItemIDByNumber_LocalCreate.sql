2c501b9f91d7efeffebee06d9e2b685a51510515
takewiki <hawken.hu@takewiki.com>
2016-08-01 00:28:37 +0800

    add the func takewiki_k3_md_customer_func_getItemIDByNumber
    add the func takewiki_k3_md_customer_func_getItemIDByNumber @takewiki

---
+++ sql/md/customer/takewiki_k3_md_customer_func_getItemIDByNumber_LocalCreate.sql
@@ -0,0 +1,9 @@
+﻿ --tsda 'takewiki_k3_md_customer_view'
+ --go
+ tsda_funcFactory_getItemIDByNumber 'takewiki_k3_md_customer_func_getItemIDByNumber',
+ 'takewiki_k3_md_customer_view',
+'FItemID',
+ 'FNumber'
+ --go
+ --select dbo.takewiki_k3_md_customer_func_getItemIDByNumber('004')
+ --select dbo.takewiki_k3_md_customer_func_getItemIDByNumber('005')

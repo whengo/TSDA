4024ae1137445eaac811b00a985b1abf527d0a1b
takewiki <hawken.hu@takewiki.com>
2016-07-31 12:15:49 +0800

    add the func takewiki_k3_md_department_func_getParentIDByLevel
    add the func takewiki_k3_md_department_func_getParentIDByLevel
    createy by pass the para to the  proc  as follows
    tsda_funcFactory_getParentIDByLevel

---
+++ sql/md/takewiki_k3_md_department_func_getParentIDByLevel_LocalCreate.sql
@@ -0,0 +1,3 @@
+﻿exec tsda_funcFactory_getParentIDByLevel 'takewiki_k3_md_department_func_getParentIDByLevel',
+'takewiki_k3_md_department_view','FItemID','FParentID','FLevel'
+-- select dbo.takewiki_k3_md_department_func_getParentIDByLevel(259,1)

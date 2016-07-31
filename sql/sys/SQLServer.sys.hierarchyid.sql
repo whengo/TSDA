55381285f4445e3f3beb38a7769f545f590baf06
takewiki <hawken.hu@takewiki.com>
2016-07-31 16:52:16 +0800

    add the comment to the hierarchyID
    由于使用了程序集，因此T

--- sql/sys/SQLServer.sys.hierarchyid.sql
+++ sql/sys/SQLServer.sys.hierarchyid.sql
@@ -65,8 +65,61 @@ WHERE EmployeeId = 4; --节点为“/1/1/”
 
 SELECT *,Org_Node.ToString() AS Org_Node_Str
 
+--ToString() 而不是tostring()区分大小写这里
 FROM dbo.Employees
 
 WHERE Org_Node.IsDescendantOf(@EmpNode)= 1;
 
+---
+DECLARE @EmpNode AS hierarchyid;
 
+SELECT @EmpNode = CAST('/' AS hierarchyid);
+
+--实现转换
+
+SELECT *,Org_Node.ToString() AS Org_Node_Str
+
+FROM dbo.Employees
+
+WHERE Org_Node.GetAncestor(2) = @EmpNode;
+
+---增加新的节点
+DECLARE @Manager AS hierarchyid, @Child1 AS hierarchyid;
+
+SELECT @Manager = Org_Node
+
+FROM dbo.Employees
+
+WHERE EmployeeId = 5; --获取部门经理B的节点
+
+SELECT @Child1 = Org_Node
+
+FROM dbo.Employees
+
+WHERE EmployeeId = 10; --获取上面语句新增员工B的节点
+
+INSERT INTO dbo.Employees VALUES --在“/1/2/1/”后面新增一个节点
+
+(@Manager.GetDescendant(@Child1, NULL), 11, '员工C');
+
+--tsda 'employees'
+
+select  org_node.toString() AS TTTT ,*  from  employees
+
+SELECT *,
+
+Org_Node.ToString() AS Org_Node_Str
+
+FROM dbo.Employees;
+
+SELECT  Org_Node.ToString() AS Org_Node_Str, *
+
+
+
+FROM dbo.Employees;
+
+---/1/2/1/
+
+
+select org_node.getDecendant().ToString()  from employees
+where employeeid=8

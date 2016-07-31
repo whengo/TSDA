71af1ce7320238f87d870e4f6e94d187cb28f25b
takewiki <hawken.hu@takewiki.com>
2016-07-31 18:13:33 +0800

    add the func
    takewiki_k3_md_department_func_getItemIDByNumber

---
+++ sql/md/takewiki_k3_md_department_func_getItemIDByNumber_LocalCreate.sql
@@ -0,0 +1,11 @@
+﻿ --tsda 'takewiki_k3_md_department_view'
+ --go
+ tsda_funcFactory_getItemIDByNumber 'takewiki_k3_md_department_func_getItemIDByNumber',
+ 'takewiki_k3_md_department_view',
+'FItemID',
+ 'FNumber'
+ --go
+ --select dbo.takewiki_k3_md_department_func_getItemIDByNumber('002')
+ --select dbo.takewiki_k3_md_department_func_getItemIDByNumber('6.08')
+ 
+ 

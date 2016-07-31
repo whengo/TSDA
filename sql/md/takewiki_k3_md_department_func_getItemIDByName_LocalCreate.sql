4cbe25093196e7c3659aace59c7b5c15def070fd
takewiki <hawken.hu@takewiki.com>
2016-07-31 18:11:48 +0800

    add the core func
    tsda_funcFactory_getItemIDByName
    tsda_funcFactory_getItemIDByNumber

---
+++ sql/md/takewiki_k3_md_department_func_getItemIDByName_LocalCreate.sql
@@ -0,0 +1,7 @@
+﻿--tsda 'takewiki_k3_md_department_view'
+
+exec tsda_funcFactory_getItemIDByName 'takewiki_k3_md_department_func_getItemIDByName',
+ 'takewiki_k3_md_department_view',
+'FItemid',
+'FName'
+--select dbo.takewiki_k3_md_department_func_getItemIDByName('生产部')

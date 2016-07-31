b2efb05ed9e317bf1f227b98c5ef4a5b05a50390
takewiki <hawken.hu@takewiki.com>
2016-07-31 12:06:12 +0800

    add the start of departemnt
    add the start of departemnt

--- bin/tsda_start_local.sql
+++ bin/tsda_start_local.sql
@@ -1,2 +1,3 @@
 exec takewiki_k3_md_COA_view_rebuild
 exec takewiki_k3_md_account_view_rebuild
+exec takewiki_k3_md_department_view_rebuild
--- bin/tsda_start_remote.sql
+++ bin/tsda_start_remote.sql
@@ -2,3 +2,4 @@ declare @DBName nvarchar(30)
 select @DBName ='数据库'
 exec takewiki_k3_md_COA_view_rebuild @DBName
 exec takewiki_k3_md_account_view_rebuild
+exec takewiki_k3_md_department_view_rebuild @DBName

e4ae22f9b2c391c4b1119e94ef46f43130e3b4e9
takewiki <hawken.hu@takewiki.com>
2016-07-31 12:43:20 +0800

    add the func 'takewiki_k3_md_department_func_getCurrentLevel'
    add the func takewiki_k3_md_department_func_getCurrentLevel
    use the core proc tsda_funcFactory_getCurrentLevel

---
+++ sql/md/takewiki_k3_md_department_func_getCurrentLevel_LocalCreate.sql
@@ -0,0 +1,7 @@
+﻿exec tsda_funcFactory_getCurrentLevel 
+'takewiki_k3_md_department_func_getCurrentLevel',
+'takewiki_k3_md_department_view','FItemID','FLevel'
+---tsda takewiki_k3_md_department_view
+
+--select dbo.takewiki_k3_md_department_func_getCurrentLevel(235)
+--select dbo.takewiki_k3_md_department_func_getCurrentLevel(255)

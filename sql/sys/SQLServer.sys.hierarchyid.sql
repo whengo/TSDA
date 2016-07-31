16a8d32c1f9d82de12c297bb7d9c8bf82886eeda
takewiki <hawken.hu@takewiki.com>
2016-07-31 16:14:57 +0800

    microsoft the new data type to deal with hierarchy data
    不怎么喜欢这种处理方式

---
+++ sql/sys/SQLServer.sys.hierarchyid.sql
@@ -0,0 +1,72 @@
+﻿--建立表结构
+IF OBJECT_ID('dbo.Employees', 'U') IS NOT NULL
+
+DROP TABLE dbo.Employees;
+
+CREATE TABLE dbo.Employees
+
+(
+
+Org_Node hierarchyid NOT NULL,
+
+EmployeeId INT NOT NULL,
+
+Title VARCHAR(50) NOT NULL,
+
+Org_Level AS Org_Node.GetLevel()
+
+);
+
+GO
+--插入数据
+INSERT INTO dbo.Employees VALUEs ('/',1,'总经理')
+
+INSERT INTO dbo.Employees VALUEs ('/1/',2,'副总经理A')
+INSERT INTO dbo.Employees VALUEs ('/2/',3,'副总经理B')
+
+INSERT INTO dbo.Employees VALUEs ('/1/1/',4,'部门经理A')
+
+INSERT INTO dbo.Employees VALUEs ('/1/2/',5,'部门经理B')
+
+INSERT INTO dbo.Employees VALUEs ('/1/1/1/',6,'员工A')
+
+--查询 
+SELECT *,
+
+Org_Node.ToString() AS Org_Node_Str
+
+FROM dbo.Employees;
+
+----查询父项节点 
+
+DECLARE @EmpNode AS  hierarchyid;
+
+SELECT @EmpNode = Org_Node
+
+FROM dbo.Employees
+
+WHERE EmployeeId = 4;
+
+SELECT *,Org_Node.ToString() AS Org_Node_Str
+
+FROM dbo.Employees
+
+WHERE @EmpNode.IsDescendantOf(Org_Node) = 1;
+
+---查询了项节点
+
+DECLARE @EmpNode AS hierarchyid;
+
+SELECT @EmpNode = Org_Node
+
+FROM dbo.Employees
+
+WHERE EmployeeId = 4; --节点为“/1/1/”
+
+SELECT *,Org_Node.ToString() AS Org_Node_Str
+
+FROM dbo.Employees
+
+WHERE Org_Node.IsDescendantOf(@EmpNode)= 1;
+
+

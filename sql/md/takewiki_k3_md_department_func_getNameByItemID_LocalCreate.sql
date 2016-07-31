60e93b762d3d5ea9cacb15c5e246d97a6fb482e2
takewiki <hawken.hu@takewiki.com>
2016-07-31 15:44:59 +0800

    add the func takewiki_k3_md_department_func_getNameByItemID
    add the func the object department  name by id

---
+++ sql/md/takewiki_k3_md_department_func_getNameByItemID_LocalCreate.sql
@@ -0,0 +1,7 @@
+﻿-- tsda 'takewiki_k3_md_department_view'
+tsda_funcFactory_getNameByItemID 'takewiki_k3_md_department_func_getNameByItemID',
+'takewiki_k3_md_department_view',
+'FItemID',
+'FName'
+--select dbo.takewiki_k3_md_department_func_getNameByItemID(234)
+--select dbo.takewiki_k3_md_department_func_getNameByItemID(243)

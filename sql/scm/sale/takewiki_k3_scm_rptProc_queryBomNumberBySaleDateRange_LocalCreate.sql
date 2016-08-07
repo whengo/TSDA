ec4f4fe40184cbfc77d315e269c05a726402c6b8
takewiki <hawken.hu@takewiki.com>
2016-08-07 12:57:26 +0800

    add the rptProc takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange
    销售管理模块与MTO计划联用的情况，根据销售订单的日期来查询BOM的录入情况，按订单的日期+订单号进行排序结果

---
+++ sql/scm/sale/takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange_LocalCreate.sql
@@ -0,0 +1,25 @@
+﻿if object_id('takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange') is not null
+drop proc takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange
+go
+create proc takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange
+(
+@StartDate datetime,
+@EndDate datetime
+)
+as
+begin
+select 
+s.FDate  销售订单日期,s.FBillNo 订单编号,ic.FNumber 物料代码,ic.FName 物料名称, ic.fmodel 规格型号,
+isnull(b.FBOMNumber,'') as  BOM编号 from SEOrder s
+inner join seorderentry se 
+on s.finterid=se.finterid
+inner join t_icitem  ic
+on se.FItemID=ic.FItemID
+left join ICBOM  b
+on se.FItemID=b.FItemID
+where s.FDate between @StartDate and @EndDate
+order by s.fdate,s.FBillNo
+end
+
+--usage
+--  exec takewiki_k3_scm_rptProc_queryBomNumberBySaleDateRange '2016-07-01','2016-07-31'

02b045219a752128d1576e49f072bf54222b3658
takewiki <hawken.hu@takewiki.com>
2016-08-07 12:47:14 +0800

    takewiki_k3_scm_rptProc_queryBomNumberBySaleOrderNumber
    根据销售订单的号码查询相应的BOM单号
    
    前提模拟：
    销售管理
    MTO计划
    生产任务管理
    流程场景：
    1）根据销售订单手工做生产任务
    2）根据销售订单跑MTO计划，然后根据 MTO计划订单投放生成相应的生产任务。

---
+++ sql/scm/sale/takewiki_k3_scm_rptProc_queryBomNumberBySaleOrderNumber_LocalCreate.sql
@@ -0,0 +1,18 @@
+﻿if object_id('takewiki_k3_scm_rptProc_queryBomNumberBySaleOrderNumber') is not null
+drop proc takewiki_k3_scm_rptProc_queryBomNumberBySaleOrderNumber
+go
+create proc takewiki_k3_scm_rptProc_queryBomNumberBySaleOrderNumber
+(@Fnumber nvarchar(30))
+as
+begin
+set nocount on
+select s.FDate 订单日期 ,s.FBillNo 订单编号,ic.FNumber 物料代码,ic.FName 物料名称,isnull(b.FBOMNumber,'') as BOM编号  from SEOrder s
+inner join seorderentry se 
+on s.finterid=se.finterid
+inner join t_icitem  ic
+on se.FItemID=ic.FItemID
+left join ICBOM  b
+on se.FItemID=b.FItemID
+where s.FBillNo =@Fnumber
+---order by s.FBillNo
+end
